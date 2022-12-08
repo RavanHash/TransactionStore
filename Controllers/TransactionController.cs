4c648ec519b43c4c1b7a202b3d1f0eed22d4fe9a
RA <User@DESKTOP-DRJMU61>
2022-12-08 14:07:25 +0400

    Add templata

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TransactionStore.Models;
+
+namespace TransactionStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private static List<Transaction> _transactions = new List<Transaction>()
+        {
+            new Transaction
+            {
+                UserId = 1,
+                AccountId = 1,
+                ReceiverId = 1,
+                TransactiontId = 1
+            },
+            new Transaction
+            {
+                UserId = 2,
+                AccountId = 2,
+                ReceiverId = 2,
+                TransactiontId = 2
+            }
+        };
+
+
+        [HttpGet("getAll")]
+        public List<Transaction> GetTransactions()
+        {
+            return _transactions;
+        }
+        
+        [HttpPost("add")]
+        public List<Transaction> PostTransactions(Transaction transaction)
+        {
+            _transactions.Add(transaction);
+            return _transactions;
+        }
+
+    }
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,16 @@
+﻿namespace TransactionStore.Models
+{
+    public class Transaction
+    {
+        public int UserId { get; set; }
+        public int AccountId { get; set; }
+        public int ReceiverId { get; set; }
+        public int TransactiontId { get; set; }
+        public enum TransactionType
+        {
+            Deposit,
+            Withdraw,
+            Transfer
+        }
+    }
+}
