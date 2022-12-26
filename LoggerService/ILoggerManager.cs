4d5d45b58b9aaf16db0175774e97d35f31f5bf2e
Ravan Hashimov <ravanhashimov@gmail.com>
2022-12-26 17:51:34 +0300

    Folder

---
+++ LoggerService/ILoggerManager.cs
@@ -0,0 +1,9 @@
+﻿namespace LoggerService;
+
+public interface ILoggerManager
+{
+    void LogError(string message);
+    void LogWarning(string message);
+    void LogDebug(string message);
+    void LogInformation(string message);
+}
---
+++ LoggerService/LoggerManager.cs
@@ -0,0 +1,27 @@
+﻿using NLog;
+
+namespace LoggerService;
+public class LoggerManager : ILoggerManager
+{
+    private static ILogger _logger = LogManager.GetCurrentClassLogger();
+
+    public void LogDebug(string message)
+    {
+        _logger.Debug(message);
+    }
+
+    public void LogError(string message)
+    {
+        _logger.Error(message);
+    }
+
+    public void LogInformation(string message)
+    {
+        _logger.Info(message);
+    }
+
+    public void LogWarning(string message)
+    {
+        _logger.Warn(message);
+    }
+}
